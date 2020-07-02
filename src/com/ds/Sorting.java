8606545f87ec8873a25b4f73a3711ac6c731428c
Akhilesh <gupta.akhilesh@siemens.com>
2020-07-02 17:42:36 +0530

    Quicksort

--- src/com/ds/Sorting.java
+++ src/com/ds/Sorting.java
@@ -4,15 +4,10 @@ public class Sorting {
 	static int a[] = { 3, 6, 5, 9, 2, 1, 8, 7, 4 };
 
 	public static void main(String[] args) {
-		int mid = a.length / 2 + a.length % 2;
-		int[] larr = new int[mid];
-		int[] rarr = new int[a.length - mid];
-		Util.split(a, larr, rarr, mid);
-		Util.traverse(larr);
-		Util.traverse(rarr);
-		merge(larr, rarr, a);
 		Util.traverse(a);
 		// mergeSort(a);
+		quickSort(0, a.length - 1, a);
+		Util.traverse(a);
 	}
 
 	public static void mergeSort(int a[]) {
@@ -22,16 +17,43 @@ public class Sorting {
 		int[] larr = new int[mid];
 		int[] rarr = new int[a.length - mid];
 		Util.split(a, larr, rarr, mid);
-		Util.traverse(larr);
-		Util.traverse(rarr);
 		mergeSort(larr);
 		mergeSort(rarr);
+		merge(larr, rarr, a);
+	}
 
+	public static void quickSort(int start, int end, int a[]) {
+		if (start < end) {
+			int pIndex = partition(a, start, end);
+			quickSort(start, pIndex - 1, a);
+			quickSort(pIndex + 1, end, a);
+		}
+	}
+
+	private static int partition(int[] a,int start,int end) {
+		int pIndex = start;
+		int i = start, temp = 0;
+		while (i < end) {
+			if (a[i] <= a[end]) {
+				temp = a[i];
+				a[i] = a[pIndex];
+				a[pIndex] = temp;
+				pIndex++;
+			}
+			i++;
+		}
+		
+		//swap pIndex with end
+		temp = a[end];
+		a[end] = a[pIndex];
+		a[pIndex] = temp;
+		
+		return pIndex;
 	}
 
 	public static void merge(int[] larr, int[] rarr, int[] marr) {
 		int i = 0, j = 0, k = 0;
-		int nL = larr.length, nR = rarr.length, nM = marr.length;
+		int nL = larr.length, nR = rarr.length;
 		while (i < nL && j < nR) {
 			if (larr[i] < rarr[j]) {
 				marr[k] = larr[i];
