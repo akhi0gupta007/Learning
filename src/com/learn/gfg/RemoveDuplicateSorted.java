ac2730f5ecb03fba5607e1a90eb6c27bc3c93621
Akhilesh <akhi0gupta007@gmail.com>
2022-08-09 11:27:01 +0530

    u

--- src/com/learn/gfg/RemoveDuplicateSorted.java
+++ src/com/learn/gfg/RemoveDuplicateSorted.java
@@ -2,4 +2,30 @@ package com.learn.gfg;
 
 public class RemoveDuplicateSorted {
 
+	public static void main(String[] args) {
+
+	}
+
+	int removeDuplicates(int arr[], int n) {
+
+		int res = arr[0];
+		int resIndex = 1;
+
+		for (int i = 1; i < n; i++) {
+			if (arr[i] != res) {
+				if (arr[resIndex] != arr[i]) {
+					arr[resIndex] = arr[i];				
+				}
+				
+				res = arr[i];
+				resIndex++;
+			} else {
+
+			}
+		}
+
+		return n;
+
+	}
+
 }
